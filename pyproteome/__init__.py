3b11dfef4984367e90dd98460f6c4c5f2e51544c
Nader Morshed <morshed.nader@gmail.com>
2016-02-02 15:57:01 -0500

    Fixed import error

--- pyproteome/__init__.py
+++ pyproteome/__init__.py
@@ -8,6 +8,7 @@ and CAMV.
 
 from .version import __version__
 from .modifications import Modification, Modifications
-from .sequence import Sequence, ProteinMatch, Motif
+from .sequence import Sequence, ProteinMatch
+from .motif import Motif
 from .protein import Protein, Proteins
 from .data_sets import DataSet
