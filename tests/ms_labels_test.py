769d7fe89aed4b2c7f7a976e18ee3fd423cb4e9d
Nader Morshed <morshed.nader@gmail.com>
2016-03-21 14:08:06 -0400

    Fixed typos

--- tests/ms_labels_test.py
+++ tests/ms_labels_test.py
@@ -6,9 +6,9 @@ from pycamv import ms_labels
 class MSLabelsTest(TestCase):
     def test_labels(self):
         for key in ms_labels.LABEL_NUMBERS.keys():
-            self.aseertIn(key, ms_labels.LABEL_MZ_WINDOW)
-            self.aseertIn(key, ms_labels.LABEL_MASSES)
-            self.aseertIn(key, ms_labels.LABEL_NAMES)
+            self.assertIn(key, ms_labels.LABEL_MZ_WINDOW)
+            self.assertIn(key, ms_labels.LABEL_MASSES)
+            self.assertIn(key, ms_labels.LABEL_NAMES)
 
         for key, val in ms_labels.LABEL_MASSES.items():
             self.assertLess(ms_labels.LABEL_MZ_WINDOW[key][0], min(val))
