9c5a78d17bd890518689c93da24aed8241f688be
Nader Morshed <morshed.nader@gmail.com>
2016-09-12 15:58:17 -0400

    Removed __init__.py imports, fixes requirement for uniprot

--- pyproteome/__init__.py
+++ pyproteome/__init__.py
@@ -7,8 +7,3 @@ and CAMV.
 """
 
 from .version import __version__
-from .modification import Modification, Modifications
-from .sequence import Sequence, ProteinMatch
-from .motif import Motif
-from .protein import Protein, Proteins
-from .data_sets import DataSet
