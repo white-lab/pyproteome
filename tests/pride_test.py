684491d8f7f1b9915a8153408d21fe576d3fc076
Nader Morshed <morshed.nader@gmail.com>
2018-01-10 23:30:37 -0800

    Added two data sources

--- tests/pride_test.py
+++ tests/pride_test.py
@@ -5,6 +5,7 @@ from pyproteome import pride
 
 class PrideTest(TestCase):
     def test_list_data(self):
+        # Raven Data
         ds = pride.list_data_set("PXD003660")
 
         self.assertEqual(
@@ -13,5 +14,17 @@ class PrideTest(TestCase):
         )
         self.assertIn(
             "20140524_MCF10A_E20VR1_ETP_TMT10.raw",
+            [i.get("name") for i in ds],
+        )
+
+        # Kristina Data
+        ds = pride.list_data_set("PXD006114")
+
+        self.assertEqual(
+            len(ds),
+            42,
+        )
+        self.assertIn(
+            "20151129_H1975HGF_VehicleControl_TMT6.mgf",
             [i.get("name") for i in ds],
         )
