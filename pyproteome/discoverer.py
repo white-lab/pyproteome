75bcc48ac5150a7e3c033b3c59b792fc3f82f331
Nader Morshed <morshed.nader@gmail.com>
2018-01-15 14:39:21 -0500

    Restored deleted function

--- pyproteome/discoverer.py
+++ pyproteome/discoverer.py
@@ -14,7 +14,7 @@ import xml.etree.ElementTree as ET
 import numpy as np
 import pandas as pd
 
-from . import fetch_data, modification, paths, protein
+from . import fetch_data, modification, paths, protein, sequence, utils
 
 
 LOGGER = logging.getLogger("pyproteome.discoverer")
@@ -60,13 +60,63 @@ def _read_peptides(conn, pick_best_ptm=False):
     return df
 
 
-def _extract_sequence(df):
-    # XXX: Hacked on...
-    from . import loading
+def _extract_sequence_prot_seq(proteins, sequence_string):
+    """
+    Extract a Sequence object from a list of proteins and sequence string.
 
+    Does not set the Sequence.modifications attribute.
+
+    Parameters
+    ----------
+    proteins : list of :class:`Protein<pyproteome.protein.Protein>`
+    sequence_string : str
+
+    Returns
+    -------
+    list of :class:`Sequence<pyproteome.sequence.Sequence>`
+    """
+    prot_matches = []
+
+    # Skip peptides with no protein matches
+    if not isinstance(proteins, protein.Proteins):
+        proteins = []
+
+    def _get_rel_pos(protein, pep_seq):
+        seq = protein.full_sequence
+
+        if not seq:
+            return 0, False
+
+        pep_pos = seq.find(pep_seq)
+        exact = True
+
+        if pep_pos < 0:
+            pep_pos = utils.fuzzy_find(pep_seq, seq)
+            exact = False
+
+        return pep_pos, exact
+
+    for prot in proteins:
+        rel_pos, exact = _get_rel_pos(prot, sequence_string.upper())
+
+        prot_matches.append(
+            sequence.ProteinMatch(
+                protein=prot,
+                rel_pos=rel_pos,
+                exact=exact,
+            )
+        )
+
+    return sequence.Sequence(
+        pep_seq=sequence_string,
+        protein_matches=prot_matches,
+    )
+
+
+def _extract_sequence(df):
     df["Sequence"] = df.apply(
         lambda row:
-        loading.extract_sequence(
+        _extract_sequence_prot_seq(
             row["Proteins"],
             row["Sequence"],
         ),
@@ -140,7 +190,7 @@ def _get_proteins(df, cursor):
         )
         sequences[peptide_id].append(seq)
 
-    # Pre-fetch UniProt data to speed up later queries
+    # Prefetch UniProt data to speed up later queries
     # fetch_data.fetch_uniprot_data(
     #     [i for lst in accessions.values() for i in lst]
     # )
