be6bfb763a7279161bce5ad649b1b336d960309e
Nader Morshed <morshed.nader@gmail.com>
2018-01-15 14:07:10 -0500

    Removed more unused code

--- pyproteome/loading.py
+++ pyproteome/loading.py
@@ -3,92 +3,17 @@ This module provides functionality for loading data sets.
 
 Functionality includes loading CAMV and MASCOT / Discoverer data sets.
 """
+
 # Built-ins
 import logging
-import os
-import re
 
 # Core data analysis libraries
 import numpy as np
-import pandas as pd
 
-from . import (
-    camv, discoverer, fetch_data, protein, sequence, modification, paths, utils
-)
+from . import camv, discoverer
 
 
 LOGGER = logging.getLogger("pyproteome.loading")
-RE_PROTEIN = re.compile("([A-Za-z0-9\(\)\[\]\\/\',\. \-\+]+) OS=")
-RE_MODIFICATION = re.compile("(N-Term|C-Term|([A-Z])([0-9]*))\((.*)\)")
-
-
-def _extract_protein(prot_string):
-    match = fetch_data.RE_ACCESSION.search(prot_string)
-    if not match:
-        raise Exception(
-            "Unable to find accession in \"{}\"".format(prot_string)
-        )
-    return protein.Protein(accession=match.group(0))
-
-
-def _extract_proteins_from_description(prots_string):
-    """
-    Extract a list of proteins from a string containing protein descriptions.
-
-    Parameters
-    ----------
-    prots_string : str
-
-    Returns
-    -------
-    :class:`Proteins<pyproteome.protein.Proteins`
-    """
-    return protein.Proteins(
-        proteins=[
-            protein.Protein(
-                accession=accession,
-            )
-            for accession in fetch_data.RE_ACCESSION.findall(prots_string)
-        ],
-    )
-
-
-
-def _extract_modification(seq, mod_string):
-    """
-    Extract a single modification from mod_string.
-
-    Parameters
-    ----------
-    seq : :class:`Sequence<pyproteome.sequence.Sequence>`
-    mod_string : str
-
-    Returns
-    -------
-    :class:`Modification<pyproteome.modification.Modification>`
-    """
-    def _get_pos(match):
-        if match.group(1) == "N-Term":
-            return 0, True, False
-        elif match.group(1) == "C-Term":
-            return len(seq.pep_seq) - 1, False, True
-        else:
-            return int(match.group(3)) - 1, False, False
-
-    match = RE_MODIFICATION.match(mod_string)
-    pos, nterm, cterm = _get_pos(match)
-    letter, mod = match.group(2), match.group(4)
-
-    if not nterm and not cterm:
-        assert seq.pep_seq[pos].upper() == letter
-
-    return modification.Modification(
-        rel_pos=pos,
-        mod_type=mod,
-        nterm=nterm,
-        cterm=cterm,
-        sequence=seq,
-    )
 
 
 def _calculate_rejected(psms, accepted, maybed, rejected):
@@ -215,21 +140,3 @@ def load_mascot_psms(basename, camv_slices=None, pick_best_ptm=False):
     psms = _calculate_accepted(psms, accepted)
 
     return psms, scan_lists, lst
-
-
-def load_validated_psms(filename):
-    """
-    Wrap load_camv_validation, returning only accepted peptides.
-
-    Parameters
-    ----------
-    filename : str
-
-    Returns
-    -------
-    :class:`pandas.DataFrame`
-    """
-    accepted, maybed, rejected = camv.load_camv_validation(filename)
-
-    accepted["Validated"] = True
-    return accepted
