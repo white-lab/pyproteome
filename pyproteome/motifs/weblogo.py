6cc4cf298cfae5d98620e087dda6cf85a44431e1
Nader Morshed <morshed.nader@gmail.com>
2018-02-14 14:33:49 -0500

    Reimplemented weblogo without biopython

--- pyproteome/motifs/weblogo.py
+++ pyproteome/motifs/weblogo.py
@@ -1,13 +1,15 @@
 import os
+import math
+import requests
 
 try:
-    import Bio.Alphabet.IUPAC
-    import Bio.Seq
-    import Bio.motifs
+    from IPython.display import Image
 except ImportError:
-    Bio = None
+    Image = None
 
 from . import motif
+
+WEBLOGO_URL = "http://weblogo.threeplusone.com/create.cgi"
 
 
 def make_logo(
@@ -32,21 +34,58 @@ def make_logo(
 
     nmer_args = motif.get_nmer_args(kwargs)
 
-    alpha = Bio.Alphabet.IUPAC.protein
-    m = Bio.motifs.create(
-        [
-            Bio.Seq.Seq(seq.upper(), alphabet=alpha)
-            for seq in motif.generate_n_mers(
-                data.psms["Sequence"],
-                **nmer_args
-            )
-        ],
-        alphabet=alpha,
+    nmers = motif.generate_n_mers(
+        data.psms["Sequence"],
+        **nmer_args
     )
-    m.weblogo(
-        filename,
-        yaxis_scale=3,
-        format="svg",
-        # logo_title="Testing",
-        stack_width="large"
+    nmer_txt = "\n".join(nmers) + "\n"
+    first_index = math.ceil(-nmer_args.get("n", 15) / 2)
+
+    response = requests.post(
+        WEBLOGO_URL,
+        files=(
+            ("sequences_file", ("", nmer_txt, "application/octet-stream")),
+            ("sequences_url", (None, "")),
+            ("sequences", (None, "")),
+            ("cmd_create", (None, "")),
+            ("logo_title", (None, data.name)),
+            ("format", (None, "png_print")),
+            ("alphabet", (None, "alphabet_auto")),
+            ("stack_width", (None, "large")),
+            ("stacks_per_line", (None, "40")),
+            ("unit_name", (None, "bits")),
+            ("first_index", (None, str(first_index))),
+            ("logo_start", (None, "")),
+            ("logo_end", (None, "")),
+            ("scale_width", (None, "true")),
+            ("composition", (None, "comp_auto")),
+            ("percentCG", (None, "")),
+            ("show_errorbars", (None, "true")),
+            # ("show_fineprint", (None, "true")),
+            ("show_xaxis", (None, "true")),
+            ("xaxis_label", (None, "")),
+            ("show_yaxis", (None, "true")),
+            ("yaxis_label", (None, "")),
+            ("yaxis_scale", (None, "auto")),
+            ("yaxis_tic_interval", (None, "1.0")),
+            ("color_scheme", (None, "color_auto")),
+            ("symbols0", (None, "")),
+            ("symbols1", (None, "")),
+            ("symbols2", (None, "")),
+            ("symbols3", (None, "")),
+            ("symbols4", (None, "")),
+            ("color0", (None, "")),
+            ("color1", (None, "")),
+            ("color2", (None, "")),
+            ("color3", (None, "")),
+            ("color4", (None, "")),
+        ),
+        stream=True,
     )
+
+    response.raise_for_status()
+
+    if Image:
+        return Image(response.raw.read(), format="png")
+    else:
+        return response.raw.read()
