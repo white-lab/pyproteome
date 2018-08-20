a5c1044fd25d9686dc7daf7beca3baf05b47a581
Nader Morshed <morshed.nader@gmail.com>
2018-08-20 18:36:44 -0400

    Removed import

--- pyproteome/analysis/__init__.py
+++ pyproteome/analysis/__init__.py
@@ -6,7 +6,7 @@ Functions include volcano plots, sorted tables, and plotting sequence
 levels.
 """
 
-from . import correlation, pathways, plot, tables, qc, volcano
+from . import correlation, plot, tables, qc, volcano
 
 __all__ = [
     "correlation",
