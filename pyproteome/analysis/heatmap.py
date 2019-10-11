Use median instead of mean for CONSTANd normalization Fixed inter / intra normalization bugs Improved several plotting functions

pyproteome/analysis/heatmap.py
@@ -7,11 +7,15 @@ import seaborn as sns
 import pyproteome as pyp
 
 
+def _zscore(x):
+    return (x - np.nanmean(x)) / np.nanstd(x)
+
+
 def hierarchical_heatmap(
     data,
     cmp_groups=None,
     baseline_channels=None,
-    minmax=None,
+    minmax=0,
     show_y=False,
     title=None,
     **kwargs
@@ -95,16 +99,13 @@ def hierarchical_heatmap(
     # raw = raw.sort_values("Fold Change", ascending=False)
     raw = raw[channels]
 
-    def zscore(x):
-        return (x - np.nanmean(x)) / np.nanstd(x)
-
-    # raw = raw.apply(zscore, axis=1)
+    # raw = raw.apply(_zscore, axis=1)
     raw = raw.apply(np.log2, axis=1)
     raw = raw.replace([np.inf, -np.inf], np.nan)
     raw = raw.dropna(how="all")
     raw = raw.T.dropna(how="all").T
 
-    if minmax is None:
+    if minmax == 0:
         minmax = max([
             abs(raw.min().min()),
             abs(raw.max().max()),

pyproteome/data_sets/constand.py
@@ -17,7 +17,13 @@ import numpy as np
 LOGGER = logging.getLogger("pyproteome.constand")
 
 
-def constand(ds, name='', inplace=False, n_iters=50, tol=1e-5):
+def constand(
+    ds, 
+    name='', 
+    inplace=False, 
+    n_iters=25, 
+    tol=1e-5,
+):
     """
     Normalize channels to given levels for intra-run comparisons.
 
@@ -53,7 +59,7 @@ def constand(ds, name='', inplace=False, n_iters=50, tol=1e-5):
     err = np.inf
     m, n = k.shape
 
-    for ind in range(1, n_iters * 2 + 1):
+    for ind in range(1, n_iters + 1):
         if ind % 2 == 1:
             # In the odd step the rows are fitted to match the row marginals
             # (i.e. constraints):
@@ -61,13 +67,15 @@ def constand(ds, name='', inplace=False, n_iters=50, tol=1e-5):
             # K^(2t + 1) = R^(t + 1) * K ^ (2t)
             #
             # The row multipliers R^(t + 1) are computed such that the mean
-            # of the reporter ion intensities equals 1/n:
+            # of the reporter ion intensities equals 1:
             r = 1 / (np.nanmean(k, axis=1))
 
             # k^(2t + 1)
+            # print(r)
             k = np.einsum('..., ...', r, k.T).T
             err = (
-                abs(np.nanmean(k, axis=0) - 1)
+                # abs(np.nanmean(k, axis=0) - 1)
+                abs(np.nanmedian(k, axis=0) - 1)
             ).sum() / 2
         else:
             # In the even step the columns are fitted to match the column
@@ -76,17 +84,21 @@ def constand(ds, name='', inplace=False, n_iters=50, tol=1e-5):
             # K^(2t + 2) = K^(2t + 1) * S^(t + 1)
             #
             # The column multipliers S^(t + 1) are computed such that the
-            # mean of the reporter ion intensities equals 1/n:
-            s = 1 / (np.nanmean(k, axis=0))
+            # mean of the reporter ion intensities equals 1:
+            s = 1 / (np.nanmedian(k, axis=0))
+            # s = 1 / (np.nanmean(k, axis=0))
+            # from pyproteome.levels import kde_max
+            # s = 1 / np.apply_along_axis(kde_max, 0, k)
 
             # k^(2t + 2)
+            # print(s)
             k = np.einsum('..., ...', k, s)
             err = (
-                abs(
-                    np.nanmean(k, axis=1) -
-                    1
-                )
+                abs(np.nanmean(k, axis=1) - 1)
+                # abs(np.nanmedian(k, axis=1) - 1)
             ).sum() / 2
+
+        # print(ind, err)
 
         if err < tol:
             break
@@ -108,7 +120,7 @@ def constand(ds, name='', inplace=False, n_iters=50, tol=1e-5):
         if key != norm_key:
             del new.psms[key]
 
-    new.intra_normalized = True
+    new.inter_normalized = True
     new.channels = new_channels
     new.groups = new.groups.copy()
 

pyproteome/data_sets/data_set.py
@@ -252,6 +252,7 @@ class DataSet:
 
             constand.constand(self, name=name, inplace=True)
             self.rename_channels(inplace=True)
+            self.inter_normalized = True
 
         if merge_duplicates:
             LOGGER.info(
@@ -611,6 +612,7 @@ class DataSet:
         -------
         ds : :class:`.DataSet`
         """
+        print('inter_normalizing', self.name, norm_channels)
         assert (
             norm_channels is not None or
             other is not None
@@ -1488,7 +1490,7 @@ class DataSet:
         """
         if groups is None:
             channel_names = [
-                self.channels[sample_name]
+                sample_name
                 for lst in self.cmp_groups or [list(self.groups.keys())]
                 for group in lst
                 for sample_name in self.groups[group]
@@ -1505,9 +1507,10 @@ class DataSet:
         ]
         d.index = self.psms.apply(
             lambda row:
-            "{} {}".format(
+            "{} {} - {}".format(
                 " / ".join(row["Proteins"].genes)[:16],
                 row["Modifications"].__str__(prot_index=0),
+                row['Sequence'],
             ),
             axis=1,
         )
@@ -1867,18 +1870,23 @@ def merge_data(
                 if sample not in new.groups[group]
             ]
 
-        # Normalize data sets to their common channels
-        if len(data_sets) > 1:
+        if not data.inter_normalized:
+            # Normalize data sets to their common channels
+            inter_norm = norm_channels
+
+            if not inter_norm:
+                inter_norm = set(data.channels)
+
+                if index > 0:
+                    inter_norm = inter_norm.intersection(new.channels)
+                elif len(data_sets) > 1:
+                    inter_norm = inter_norm.intersection(
+                        data_sets[1].channels
+                    )
+
             data = data.inter_normalize(
                 other=new if index > 0 else None,
-                norm_channels=(
-                    norm_channels
-                    if norm_channels else (
-                        set(data.channels).intersection(new.channels)
-                        if index > 0 else
-                        set(data.channels).intersection(data_sets[1].channels)
-                    )
-                ),
+                norm_channels=inter_norm,
             )
 
         for key, val in data.channels.items():
@@ -1895,6 +1903,7 @@ def merge_data(
             new.merge_duplicates(inplace=True)
 
     new.sets = sum(data.sets for data in data_sets)
+    new.inter_normalized = True
 
     new.group_a = next(
         (i.group_a for i in data_sets if i.group_a is not None),
@@ -2066,7 +2075,7 @@ def update_correlation(ds, corr, metric="spearman", min_periods=5):
 
     vals[
         (~df.isnull()).sum(axis=1) < min_periods
-    ] = [np.nan, np.nan]
+    ] = np.nan
 
     ds.psms[['Fold Change', 'p-value']] = vals
 

pyproteome/discoverer/__init__.py
@@ -642,7 +642,7 @@ def _get_quantifications(df, cursor, pd_version, tag_names):
             for val in vals
         ]
 
-        if not np.isnan(vals).all() and not row["Sequence"].is_labeled:
+        if not row["Sequence"].is_labeled:
             vals = [np.nan] * len(vals)
 
         return pd.Series(vals, index=col_names)