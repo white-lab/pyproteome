2933bfd7b75dd6653338ce588d4e2da46d921f4e
Nader Morshed <morshed.nader@gmail.com>
2020-07-12 13:05:24 -0400

    Added beginnings of plsr and go modules

---
+++ pyproteome/pathways/go.py
@@ -0,0 +1,65 @@
+
+from goatools.base import download_go_basic_obo
+from goatools.base import download_ncbi_associations
+from goatools.go_search import GoSearch
+from goatools.anno.genetogo_reader import Gene2GoReader
+from collections import defaultdict
+
+import importlib
+import re
+
+TAXA = {
+    'Mus musculus': 10090,
+    'Homo sapiens': 9606,
+}
+
+def get_go_ids(go_ids, species='Homo sapiens'):
+    assert species in TAXA
+
+    if isinstance(go_ids, str):
+        go_ids = [go_ids]
+
+    obo_fname = download_go_basic_obo("db/go/go-basic.obo")
+    gene2go = download_ncbi_associations("db/go/gene2go")
+
+    taxid = TAXA[species]
+
+    fin_symbols = "genes_NCBI_{TAXID}_All.py".format(TAXID=taxid)
+
+    module_name = "".join(["goatools.test_data.", fin_symbols[:-3]])
+    module = importlib.import_module(module_name)
+    GeneID2nt = module.GENEID2NT
+
+    go2geneids = Gene2GoReader(
+        "db/go/gene2go",
+        taxids=[taxid],
+    )
+
+    go2items = defaultdict(list)
+    for i in go2geneids.taxid2asscs[taxid]:
+        go2items[i.GO_ID].append(i.DB_ID)
+
+    srchhelp = GoSearch("db/go/go-basic.obo", go2items=go2items)
+
+    with open("go.log", "w") as log:
+        # Add children GOs
+        gos_all = srchhelp.add_children_gos(go_ids)
+        
+        # Get Entrez GeneIDs for cell cycle GOs
+        gene_ids = set()
+
+        for go_items in [
+            go_ids,
+            gos_all,
+        ]:
+            gene_ids.update(srchhelp.get_items(go_items))
+
+    genes = []
+
+    for geneid in gene_ids:
+        nt = GeneID2nt.get(geneid, None)
+
+        if nt is not None:
+            genes.append(nt.Symbol)
+
+    return genes
---
+++ pyproteome/pathways/plsr.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def vip(model):
+    try:
+        t = model.x_scores_
+        w = model.x_weights_
+        q = model.y_loadings_
+    except:
+        t = model.x_scores
+        w = model.x_weights
+        q = model.y_loadings
+    
+    p, h = w.shape
+    vips = np.zeros((p,))
+    s = np.diag(t.T @ t @ q.T @ q).reshape(h, -1)
+    total_s = np.sum(s)
+    
+    for i in range(p):
+        weight = np.array([
+            (w[i, j] / np.linalg.norm(w[:, j])) ** 2
+            for j in range(h)
+        ])
+        vips[i] = np.sqrt(p*(s.T @ weight) / total_s)
+
+    return vips
